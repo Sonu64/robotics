9e505c56680076b3e9055ccc6c6ed3ec799b59d9
SOURAKANTI MANDAL <sonusantu64@gmail.com>
2026-04-01 11:22:37 +0530

    Add input handling for motor speed and battery level with validation

--- cpp_for_robotics/06_08_string_stream.cpp
+++ cpp_for_robotics/06_08_string_stream.cpp
@@ -11,6 +11,26 @@ int main() {
 
     myStream << "Enter Motor Speed (m/s) and Battery Level of the Robot\n";
 
+    string prompt = myStream.str();
+    cout << prompt;
+
+    cin >> motorSpeed;
+    if (!cin) {
+        cerr << "Please provide Valid Input !";
+        return 1;
+    }
+
+    cin >> batteryLevel;
+        if (!cin) {
+        cerr << "Please provide Valid Input !";
+        return 1;
+    }
+
+
+    myStream << "Robot Details\nMotor Speed: " << motorSpeed << "m/s, Battery Level: " << batteryLevel << endl;
+
+    cout << myStream.str();
     
+    return 0;
 
 }
