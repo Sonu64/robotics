f392100098af8fb8d81f2a5f694970926186a377
SOURAKANTI MANDAL <sonusantu64@gmail.com>
2026-04-01 11:08:53 +0530

    see next commit msg...this ones for temporary pushing to github and code in windows currently as the Ubuntu VM is lagging.

---
+++ cpp_for_robotics/06_08_string_stream.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    stringstream myStream;
+    int motorSpeed;
+    double batteryLevel;
+
+    myStream << "Enter Motor Speed (m/s) and Battery Level of the Robot\n";
+
+    
+
+}
