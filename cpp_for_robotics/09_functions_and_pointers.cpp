773ecb4e98e5c3d12b9fc33c5562bdcd96370dc8
SOURAKANTI MANDAL <sonusantu64@gmail.com>
2026-04-04 16:39:45 +0530

    Added Basic Pointer usage Code and explanatory comments

--- cpp_for_robotics/09_functions_and_pointers.cpp
+++ cpp_for_robotics/09_functions_and_pointers.cpp
@@ -8,7 +8,18 @@ using namespace std;
 // Function prototype, Declaration
 double calculate_distance(double x1, double y1, double x2, double y2);
 
+
+
+
+
+
+
+
+
+
 int main() {
+
+    // C++ provided functions, like from the cmath lib.
     double angle = 45.0;
     double radians = angle * M_PI / 180.0;
  
@@ -21,15 +32,58 @@ int main() {
     cout << "Sine: " << sine << endl;
     cout << "Cosine: " << cosine << endl;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Usage of User-defined Functions
     double required_distance = calculate_distance(0, 0, 12.5, 10);
     stringstream response_builder;
     response_builder << "Distance = " << required_distance;
     string response = response_builder.str();
     cout << response << endl;
 
+
+
+
+
+
+
+
+    // Pointers, an easy way to understand the basics can be as follows... 
+    // '*' stands for value at address 
+    // '&' stands for address of
+    int robot_id = 42;
+    int* ptr = &robot_id;
+ 
+    cout << "Robot ID: " << robot_id << endl;
+    cout << "Pointer Value: " << ptr << endl;
+    cout << "Dereferenced Pointer: " << *ptr << endl;
+ 
+    *ptr = 99;
+    cout << "Updated Robot ID: " << robot_id << endl;
+
     return 0;
 }
 
+
+
+
+
+
+
+
+
+// Function Definition.
 // Function that calculates distance using your fav Distance Formula !
 double calculate_distance(double x1, double y1, double x2, double y2) {
     double dx = x2 - x1;
