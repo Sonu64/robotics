9c0013b2b19101a328c5e78ae6d7b3828595beca
SOURAKANTI MANDAL <sonusantu64@gmail.com>
2026-04-15 16:39:41 +0530

    Wrapped up OOPs mostly, some more C++ syntax practiced, little tweaks checked.

--- cpp_for_robotics/13_robot_actuators.cpp
+++ cpp_for_robotics/13_robot_actuators.cpp
@@ -1,4 +1,4 @@
-/// @brief This code demonstrates Polymorphism in C++, a very important OOP concept.
+/// @brief This code demonstrates Inheritance and Polymorphism in C++, a very important OOP concept.
 
 #include <iostream>
 #include <string>
@@ -6,11 +6,14 @@
 
 using namespace std;
 
+/// @brief  "pure" ABSTRACT in C++ is equivalen to Java Interface. An abstract class is a class that cannot be directly used to create an object. 
+// So, "pure" ABSTRACT (all  = 0) and "partially" ABSTRACT (some = 0) classes are used to define blueprints that other classes can implement. Neither can be used to directly create objects. Objects are instantiable via classes with all -> Defined !!!
 class ActuatorInterface {
     public:
     virtual void activate() = 0; // Pure virtual function making this class an abstract class
 };
 
+// Motor and Servo both are inheriting from the ActuatorInterface and providing their own implementation of the activate function
 class Motor : public ActuatorInterface {
     public:
     void activate() override { // Override the pure virtual function from the ActuatorInterface
@@ -36,6 +39,12 @@ int main() {
 
     Motor motor;
     Servo servo;
+    Motor* dynamicMotor = new Motor(); // Dynamically allocated Motor object on the heap
+    (*dynamicMotor).activate(); // Activating the motor using the dynamically allocated object
+    dynamicMotor->activate(); // Activating the motor using the pointer to the dynamically allocated object // Both syntaxes work ! But this ones used more commonly in C++ for accessing members of a class through a pointer
+
+    /// @important: Always remember to deallocate memory for dynamically allocated objects to prevent memory leaks. In this case, we should delete the dynamicMotor object after we are done using it.
+    delete dynamicMotor;
 
     testActuator(motor);
     testActuator(servo);
