46bacd37c06f922f3b453582bcf17f1fa89f734d
Sonu64 <sonusantu64@gmail.com>
2026-04-21 16:32:19 +0530

    More Lambdas...

--- cpp_for_robotics/16_lambda_in_detail.cpp
+++ cpp_for_robotics/16_lambda_in_detail.cpp
@@ -8,15 +8,50 @@ void printElement(int x) {
     cout << x << " ";
 }
 
+void check(int x) {
+    (x%2 == 0) ? cout << "Even\t" : cout << "Odd\t";
+}
+
 int main() {
 
     vector<int> v{2, 4, 56, 21, 90};
+
+    int denominator;
+    cout << "Enter the denominator: ";
+    cin >> denominator;
 
     cout << "Using a normal function:" << endl;
     for_each(v.begin(), v.end(), printElement);
 
     cout << "\nUsing Lambda Expression:" << endl;
     for_each(v.begin(), v.end(), [] (int x) {cout << x << " ";});
+
+    //checking even odd by normal function
+    cout << "\nChecking for Even Odd by normal function:" << endl;
+    for_each(v.begin(), v.end(), check);
+
+    cout << "\nUsing Lambda Expression, copying each element." << endl;
+    for_each(v.begin(), v.end(), [](int& x){(x%2 == 0) ? cout << "Even\t" : cout << "Odd\t";}); // int a copies each element
+
+    cout << "\nUsing Lambda Expression, avoiding copying -- Things can be changed by ref here !" << endl;
+    for_each(v.begin(), v.end(), [](int& x){(x%2 == 0) ? cout << "Even\t" : cout << "Odd\t";}); // int& a avoids copying.
+
+    cout << "\nUsing Lambda Expression, avoiding copying -- Things can't be changed here, as we use const !'" << endl;
+    for_each(v.begin(), v.end(), [](const int& x){(x%2 == 0) ? cout << "Even\t" : cout << "Odd\t";}); // const int& a avoids copying and also enforces our "intention" of not doing something to the original number in an even-odd checker, into an "Enforcement" !
+
+
+    cout << "\nCapturing an external variable by value:" << endl;
+    for_each(v.begin(), v.end(), [denominator](int x){
+        (x%denominator==0) ? cout << "Divisible\t" : cout << "Not Divisible\t";
+    });
+
+    cout << "\nCapturing an external variable by reference, also paramter passed by ref and marked const --> BEST PRACTICE IN ROBOTICS !" << endl;
+    for_each(v.begin(), v.end(), [&denominator](const int& x){
+        (x%denominator==0) ? cout << "Divisible\t" : cout << "Not Divisible\t";
+    });
+
+    
+
 
     // [capture_clause] (params) {func_def}
 
