648ea124c8245b86cc86f665684aba548d12bc84
Sonu64 <sonusantu64@gmail.com>
2026-04-22 19:35:16 +0530

    Learnt Range Based for and for_each+lambdas.

--- cpp_for_robotics/17_lambdas_and_fileio.cpp
+++ cpp_for_robotics/17_lambdas_and_fileio.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main() {
     vector<int> sensor_readings{45, 89, 12, 47, 65};
 
+    /* DEMONSTRATION OF LAMBDA EXPRESSIONS IN C++, TO SORT READINGS IN DESCENDING ORDER */
     // We will use lamdba expression in the sort algo to sort in Descending order
     sort(sensor_readings.begin(), sensor_readings.end(), [](int a, int  b){
         return a > b;
@@ -16,6 +17,32 @@ int main() {
     for_each(sensor_readings.begin(), sensor_readings.end(), [](int x){
         cout << x << " ";
     });
+
+
+
+
+    /* DEMONSTRATION OF FOR_EACH LOOP */
+    vector<int> distances = {100, 568, 980, 145, 321};
+
+    // Below syntax is also RANGE_BASED_FOR
+        cout << "\nOriginal distances - " << endl;
+        for(int distance : distances) {
+            cout << distance << " ";
+        }
+
+
+    // Below syntax is FOR_EACH, combined with lambda expressions.
+    const int amount = 10;
+    for_each(distances.begin(), distances.end(), [amount](int& distance){
+        distance += amount;
+    });
+
+
+    cout << "\nUpdated distances - " << endl;
+    for(int distance : distances) {
+            cout << distance << " ";
+    }
+
 
     cout << endl;
 
