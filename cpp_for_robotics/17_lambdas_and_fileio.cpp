8a3ca44936cf1236530644880c553cdd59739afe
Sonu64 <sonusantu64@gmail.com>
2026-04-22 19:48:08 +0530

    Wrapped up Some Important File I/O concepts.

--- cpp_for_robotics/17_lambdas_and_fileio.cpp
+++ cpp_for_robotics/17_lambdas_and_fileio.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -44,6 +46,37 @@ int main() {
     }
 
 
+
+    /* DEMONSTRATION OF FILE HANDLING*/
+ 
+    string filename = "robot_data.txt";
+    ofstream output_file(filename);
+ 
+
+    // Checking wheather the File opening was sucessful or not is Must using the is_open() method of ofstream, before writing anything in it using Multiline Insertion operators - '<<'.
+    if (output_file.is_open()) {
+        output_file << "Sensor1: 10.5\n";
+        output_file << "Sensor2: 20.7\n";
+        output_file << "Sensor3: 15.2\n";
+        output_file.close();
+        cout << "Data written to file: " << filename << std::endl;
+    } else {
+        cout << "Unable to open file for writing." << std::endl;
+    }
+ 
+    ifstream input_file(filename);
+    string line;
+ 
+    // Same precaution here ! Before reading every line, check wheather the file successfuly opened or not.
+    if (input_file.is_open()) {
+        while (getline(input_file, line)) {
+            cout << line << endl;
+        }
+        input_file.close();
+    } else {
+        cout << "Unable to open file for reading." << endl;
+    }
+ 
     cout << endl;
 
 }
