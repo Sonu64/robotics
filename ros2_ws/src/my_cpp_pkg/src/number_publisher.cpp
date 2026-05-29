5fbb02098e41a611475c108b43387735d35e1cf6
SOURAKANTI MANDAL <sonusantu64@gmail.com>
2026-05-29 20:53:11 +0530

    Add NumberPublisherNode to publish Int64 messages at regular intervals

---
+++ ros2_ws/src/my_cpp_pkg/src/number_publisher.cpp
@@ -0,0 +1,37 @@
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/int64.hpp"
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+using Integer64 = example_interfaces::msg::Int64; // Type alias for the Int64 message
+
+class NumberPublisherNode : public rclcpp::Node
+{
+public:
+    NumberPublisherNode() : Node("number_publisher"), count_(0) {
+        publisher_ = this->create_publisher<Integer64>("number", 10);
+        timer_ = this->create_wall_timer(1s, std::bind(&NumberPublisherNode::publish_number, this));
+        RCLCPP_WARN(this->get_logger(), "Number Publisher Node has started publishing numbers...");
+    }
+ 
+private:
+    rclcpp::Publisher<Integer64>::SharedPtr publisher_;
+    int64_t count_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    void publish_number() {
+        auto msg = Integer64();
+        msg.data = count_;
+        this->publisher_->publish(msg); // remember that publisher_ is a Shared Pointer, so we use the -> operator to call the publish method on it.
+        count_++;
+    }
+};
+ 
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<NumberPublisherNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
