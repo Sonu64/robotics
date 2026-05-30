985bc560b9a6fb1885d7c9a18e7881362617031e
SOURAKANTI MANDAL <sonusantu64@gmail.com>
2026-05-30 10:18:30 +0530

    Assignment done for Section-3

--- ros2_ws/src/my_cpp_pkg/src/number_counter.cpp
+++ ros2_ws/src/my_cpp_pkg/src/number_counter.cpp
@@ -2,22 +2,50 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
 
+using namespace std::chrono_literals;
+using namespace std::placeholders; 
+// This is needed to use std::bind with member functions. It allows us to use _1, _2, etc. as placeholders for the arguments that will be passed to the callback function when it is called by the ROS 2 executor.
+
 using Integer64 = example_interfaces::msg::Int64; // Type alias for the Int64 message
 
-class NumberCounterNode : public rclcpp::Node { 
+class NumberCounterNode : public rclcpp::Node {
+    public: 
+        NumberCounterNode() : Node("number_counter"), number_count_(0) {
+            this->subscriber_ = this->create_subscription<Integer64>(
+            "number", 10, std::bind(&NumberCounterNode::get_number_callback, this, _1)
+        );
 
-    public:
+        this->publisher_ = this->create_publisher<Integer64>("number_count", 10);
+
+        this->timer_ = this->create_wall_timer(1s, std::bind(&NumberCounterNode::publish_number_callback, this));
+
+        RCLCPP_WARN(this->get_logger(), "Number Counter has started Listening to /number and Simultaneosly publishing to the /number_count");
+
+        }
 
     private:
+        int64_t number_count_;
+        rclcpp::Subscription<Integer64>::SharedPtr subscriber_;
+         rclcpp::Publisher<Integer64>::SharedPtr publisher_;
+        rclcpp::TimerBase::SharedPtr timer_;
+
+        void get_number_callback(const Integer64::SharedPtr msg) {
+            number_count_ = msg->data; // Update the count with the received number
+            RCLCPP_WARN(this->get_logger(), "Received number: %ld", number_count_);
+        }
+
+        void publish_number_callback() {
+            auto msg = Integer64();
+            msg.data  = number_count_; // Publish the count of numbers received
+            this->publisher_->publish(msg);
+        }
+
         
-
-}
-
-
+    };
 
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<MyCustomNode>(); // MODIFY NAME
+    auto node = std::make_shared<NumberCounterNode>(); // MODIFY NAME
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
