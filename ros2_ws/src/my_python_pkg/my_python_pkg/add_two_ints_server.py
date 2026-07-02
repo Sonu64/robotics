fe48d78346ac2920370196c3f1fba6884775c7b9
SOURAKANTI MANDAL <sonusantu64@gmail.com>
2026-07-02 15:17:19 +0530

    Rename add_two_ints service to add_two_ints_server and update setup.py; add add_two_ints_client_no_oop.py file

--- ros2_ws/src/my_python_pkg/my_python_pkg/add_two_ints_server.py
+++ ros2_ws/src/my_python_pkg/my_python_pkg/add_two_ints_server.py
@@ -6,7 +6,7 @@ from example_interfaces.srv import AddTwoInts
  
 class AddTwoIntsServerNode(Node): 
     def __init__(self):
-        super().__init__("add_two_ints_node")
+        super().__init__("add_two_ints_server")
         self.server_ = self.create_service(AddTwoInts, "add_two_ints", self.callback_add_two_ints)
         self.get_logger().warning("Add Two Ints Server has been started...")
 
@@ -18,14 +18,12 @@ class AddTwoIntsServerNode(Node):
         return response
 
 
-
  
 def main(args=None):
     rclpy.init(args=args)
     node = AddTwoIntsServerNode() 
     rclpy.spin(node)
     rclpy.shutdown()
- 
  
 if __name__ == "__main__":
     main()
