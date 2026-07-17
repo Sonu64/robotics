8469333d6aa68a14f87a519896459403a9435609
SOURAKANTI MANDAL <sonusantu64@gmail.com>
2026-07-17 15:11:14 +0530

    time to go to duel booted Ubuntu...

--- ros2_crash_course_ws/src/cpp_pkg/src/cpp_pub.cpp
+++ ros2_crash_course_ws/src/cpp_pkg/src/cpp_pub.cpp
@@ -1,10 +1,39 @@
-#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/string.hpp"
+#include <string>
+#include <memory>
+#include <chrono>
 
-int main(int argc, char ** argv)
+using namespace std::chrono_literals;
+
+using String = example_interfaces::msg::String; // This is just a type alias to make the code cleaner and easier to read. It allows us to use String instead of the full example_interfaces::msg::String every time we want to refer to that message type.
+ 
+
+class MinimalPublisher : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+public:
+    MinimalPublisher() : Node("minimal_publisher"), count(0)  {
+      publisher_ = this->create_publisher<String>("scribble", 10);
+      timer_ = this->create_wall_timer(100ms, std::bind(&MinimalPublisher::timer_callback, this));
+      RCLCPP_INFO(this->get_logger(), "Minimal C++ Publisher has  started publishing messages.");
+    }
+ 
+private:
+    rclcpp::Publisher<String>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    size_t count;
 
-  printf("hello world cpp_pkg package\n");
-  return 0;
+    void timer_callback() {
+      auto msg = String();
+      msg.data = "Hello, World! " + std::to_string(count++); // print and increment the count variable, which is a member variable of the class. This allows us to keep track of how many messages have been published so far.
+      publisher_->publish(msg);
+    }
+};
+ 
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<MinimalPublisher>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
---
+++ ros2_crash_course_ws/src/cpp_pkg/src/cpp_sub.cpp
@@ -0,0 +1,34 @@
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/string.hpp"
+
+
+using namespace std::chrono_literals;
+using namespace std::placeholders; 
+// This is needed to use std::bind with member functions. It allows us to use _1, _2, etc. as placeholders for the arguments that will be passed to the callback function when it is called by the ROS 2 executor.
+
+using String = example_interfaces::msg::String;
+ 
+
+class MinimalSubscriber : public rclcpp::Node
+{
+public:
+    MinimalSubscriber() : Node("minimal_subscriber"), count(0)  {
+      subscription_ = this->create_subscription<String>("scribble", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+      RCLCPP_INFO(this->get_logger(), "Minimal C++ Subscriber has  started listening for messages.");
+    }
+ 
+private:
+    rclcpp::Subscription<String>::SharedPtr subscription_;
+
+    void topic_callback(const String::SharedPtr msg) {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    }
+};
+ 
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<MinimalSubscriber>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
