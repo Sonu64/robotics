00299507af6cddfb618cd4aa1d37ae113371a62d
Sonu <sonusantu64@gmail.com>
2026-08-01 10:42:01 +0530

    ROS2 Parameters started pretty well.

--- ros2_ws/src/my_python_pkg/my_python_pkg/number_publisher.py
+++ ros2_ws/src/my_python_pkg/my_python_pkg/number_publisher.py
@@ -7,9 +7,15 @@ from example_interfaces.msg import Int64
 class NumberPublisherNode(Node):
     def __init__(self):
         super().__init__("number_publisher") 
-        self.count_ = 0
+
+        # ROS2 Parameters below !
+        self.declare_parameter("count", 0) # int
+        self.declare_parameter("timer_period", 1.0) # float
+        self.count_ = self.get_parameter("count").value
+        self.timer_period_ = self.get_parameter("timer_period").value
+        
         self.publisher_ = self.create_publisher(Int64, "number", 10)
-        self.timer_ = self.create_timer(1, self.publish_number_callback)
+        self.timer_ = self.create_timer(self.timer_period_, self.publish_number_callback)
         self.get_logger().warning("Python Number Publisher has started publishing...")
     
     def publish_number_callback(self):
