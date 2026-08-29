c13f99bf6694db4ecd9c89a77eb8a306f11196c5
Sourakanti Mandal <sonusantu64@gmail.com>
2026-08-29 12:50:17 +0530

    Now turtles spawn at random coordinates continuosly based on a 1s timer.

--- ros2_ws/src/turtlesim_catch_them_all/src/turtle_spawner.cpp
+++ ros2_ws/src/turtlesim_catch_them_all/src/turtle_spawner.cpp
@@ -6,11 +6,24 @@
 
 class TurtleSpawner : public rclcpp::Node {
 public:
+
     TurtleSpawner() : Node("turtle_spawner")  { 
         RCLCPP_INFO(this->get_logger(), "TurtleSpawner node has been started.");
         alive_turtles_pub_ = this->create_publisher<turtlesim::msg::Pose>("alive_turtles", 10);
+        
+        timer_ = this->create_wall_timer(
+      std::chrono::seconds(1),
+      std::bind(&TurtleSpawner::spawnTurtle, this));
     }
-     void spawnTurtle(double x, double y, double theta) {
+
+
+     void spawnTurtle() {
+        
+        double x = static_cast<double>(rand()) / RAND_MAX * 10.0;
+        double y = static_cast<double>(rand()) / RAND_MAX * 10.0;
+        double theta = static_cast<double>(rand()) / RAND_MAX * 2.0 * 3.14159; // Random angle between 0 and 2π
+
+
         auto client = this->create_client<turtlesim::srv::Spawn>("spawn");
         while (!client->wait_for_service(std::chrono::seconds(1))) {
             RCLCPP_WARN(this->get_logger(), "Spawn service not available, waiting again...");
@@ -27,12 +40,14 @@ public:
         last_spawned_turtle_pose_.theta = theta;
         alive_turtles_pub_->publish(last_spawned_turtle_pose_);
 
+
         //..publish to topic here !
         alive_turtles_pub_->publish(last_spawned_turtle_pose_);
         
     }
 private:
 
+    rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<turtlesim::msg::Pose>::SharedPtr alive_turtles_pub_;
     turtlesim::msg::Pose last_spawned_turtle_pose_;
 
@@ -46,10 +61,7 @@ private:
  
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<TurtleSpawner>();
-    node->spawnTurtle(5.0, 5.0, 0.0);
-    node->spawnTurtle(2.0, 3.0, 1.57);
-    node->spawnTurtle(8.0, 1.0, 3.14);
+    auto node = std::make_shared<TurtleSpawner>();  
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
